ae19d0e6dacb97e245c4324a24fbb4d1c997052b
Yarden Cohen <57362284+yarden7696@users.noreply.github.com>
2020-08-29 13:37:11 +0300

    Update server.c

--- q_2/q22/server.c
+++ q_2/q22/server.c
@@ -16,16 +16,13 @@ void _handler(int signal) {
     if(signal==2)
 	 	counter++; // Counting the number of SIGINT signals that received to the server 
 		 
-	else if(signal==10)  
+    else if(signal==10)  
 	    // Print the number of SIGINT signals that have reached the server
 		printf("\n A total of %d SIGINT signals reached to the server \n",counter);	
 }
 
-
 int main() {
-	
 	printf("Server`s PID : %d\n", getpid()); // print server`s pid to the terminal	
-
 	while(1) {  // getting signals that sending by the client 
 		signal(SIGUSR1,_handler);
 		signal(SIGINT,_handler);
